d381e4642eae1a622bf0354fe8a525ceb0921192
shivamkumraa9 <shivamkumraa9@gmail.com>
2020-07-20 13:32:46 +0530

    Deploy

--- payments/models.py
+++ payments/models.py
@@ -12,7 +12,7 @@ class Customer(models.Model):
 
 
 class Intent(models.Model):
-	intent = models.CharField(max_length=255,unique = True)
+	intent = models.TextField(unique = True)
 
 	def __str__(self):
 		return self.intent
